91b4a5a779c22dc6d0f13b4880c303bae8cb4612
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:32:14 +0530

    Update program006.c

--- program006.c
+++ program006.c
@@ -1,43 +1,31 @@
-// Input : 4
-// Output : 
-/*
-4
-3
-2
-1
-0
-*/
-
-#include<stdio.h>
+/////////////////////////////////////////////
+// 
+//   File name: Program.c   
+//   Desription : 
+//   Author : Nikhil Kailas Gambhir
+//   Date : 9/05/2025
+//
+///////////////////////////////////////////////#include<stdio.h>
 
 void Display(int iNo)
 {
     int iCnt = 0;
-    
-    if(iNo < 0)
+    //write updater
+    while (iCnt< iNo)
     {
-        iNo = -iNo;
-    } 
-    
-    for(iCnt = iNo; iCnt >= 0; iCnt--)
-    {
-        printf("%d\n",iCnt);
+        printf("*\n");//
+        iCnt++;
     }
-    printf("\n");
+    
 }
 
 int main()
-{
+{ 
     int iValue = 0;
 
-    printf("Enter frequency : \n");
-    scanf("%d",&iValue);
+    printf("Enter the number\n");//
+    scanf("%d", &iValue);//
 
     Display(iValue);
-
     return 0;
 }
-
-// Time Complexity O(N)
-// N is a natural number
-// Where N >= 0 and N <= Inifinity
