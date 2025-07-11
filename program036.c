a3f0afcb84cceed7f8c4fb38482f7e1f4f2210ce
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:51:55 +0530

    Update program036.c

--- program036.c
+++ program036.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
-// iteration
-void Display(int iNo)
-{
-    int iCnt = 0;
-    
-    for(iCnt = 1; iCnt <=iNo; iCnt++)
-{
-     printf("Hello\n");
-}
-}
-int main()
-{
-    int iValue = 0;
+#define PI 3.14
 
-  
-    printf(" Entear the frequency :\n");
-    scanf("%d",&iValue);
-    Display(iValue);
-
-    return 0;
+double CircleArea(float fRadius)
+{
+    return PI * fRadius * fRadius;
 }
+  int main()
+  {
+     float fValue = 0.0;
+     double dRet = 0.0;
+
+     printf("Enter radius");
+     scanf("%f",&fValue);
+
+     dRet = CircleArea(fValue);
+
+     printf("Area of circle is: %lf", dRet);
+
+     return 0;
+  }
