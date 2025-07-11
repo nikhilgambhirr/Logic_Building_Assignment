525bd087ebf234d924671a59df8df9b10ec80419
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:57:48 +0530

    Update program046.c

--- program046.c
+++ program046.c
@@ -1,44 +1,36 @@
 #include<stdio.h>
 
-#define ERR_INVALID -1
-
-unsigned long Factorial(unsigned int iNo)
+int CountEven(int iNo)
 {
-    int iCnt = 0;
-    unsigned long iFact = 1;
+    int iDigit = 0;
+    int iCount = 0;
 
-    if(iNo < 0)
+    if( iNo < 0)
     {
-        return ERR_INVALID;
+        iNo = -iNo;
     }
-    
-    iCnt = 1;
-    while( iCnt <= iNo )
-    {
-        iFact = iFact * iCnt;
-        iCnt++;
-    }
-
-    return iFact;
+     while (iNo != 0)
+     {
+         iDigit = iNo % 10;
+         if(iDigit % 2 ==0)
+         {
+            iCount++;
+         }
+          iNo = iNo / 10;
+     }
+     
+     return iCount;
 }
-
 int main()
 {
-    unsigned int iValue = 0;
-    unsigned long iRet = 0;
-    
-    printf("Enter number : \n");
-    scanf("%u",&iValue);
+    int iValue = 0;
+    int iRet = 0;
 
-    iRet = Factorial(iValue);
-    if(iRet == ERR_INVALID)
-    {
-        printf("Invalid input\n");
-    }
-    else
-    {
-        printf("Factorial is : %lu\n",iRet);
-    }
-    
+    printf("Enter number:\n");
+    scanf("%d",&iValue);
+
+    iRet = CountEven(iValue);
+    printf("count of even digits :%d\n",iRet);
+
     return 0;
-}
+ }
