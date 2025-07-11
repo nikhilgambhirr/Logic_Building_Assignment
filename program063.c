3dd47e8710e4bf22232468ea687222311ad11d4b
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-12 00:14:05 +0530

    Update program063.cjava

--- program063.c
+++ program063.c
@@ -1,50 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
+// input 3  4
+/*
+   A  A  A  A
+   B  B  B  B
+   C  C  C  C 
+ */
 
-int SumEven(int Arr[], int iSize)
+import java.util.Scanner;
+
+class Layout
 {
-    int iCnt = 0, iSum = 0;
-
-    for(iCnt = 0; iCnt < iSize; iCnt++)
+    public void Display(int iRow, int iCol)
     {
-        if((Arr[iCnt] % 2) == 0)
+        int i = 0, j = 0;
+        char ch = 'A';
+
+        for(i = 1, ch = 'A'; i <= iRow; i++,ch++)
         {
-            iSum = iSum + Arr[iCnt];
+            for(j = 1;j <= iCol; j++)
+                {
+                    System.out.print(ch+"\t");
+                }
+                   System.out.println();
         }
     }
-
-    return iSum;
 }
-
-int main()
+class program63
+ {
+     public static void main(String A[])
 {
-    int iLength = 0, iCnt = 0, iRet = 0;
-    int *ptr = NULL;
+    Scanner sobj = new Scanner(System.in);
+    int iValue1 = 0, iValue2 = 0;
 
-    printf("Enter the number of elements : \n");
-    scanf("%d",&iLength);
+    System.out.println("Enter number of rows :");
+    iValue1 = sobj.nextInt();
 
-    // Step 1 : Allocate the memory
-    ptr = (int *)malloc(iLength * sizeof(int));
-    if(ptr == NULL)
-    {
-        printf("Unable to allocate memory\n");
-        return -1;
-    }
+    System.out.println("Enter number of colum :"); 
+    iValue2 = sobj.nextInt();
 
-    printf("Enter the elements : \n");
-    for(iCnt = 0; iCnt < iLength; iCnt++)
-    {
-        scanf("%d",&ptr[iCnt]);
-    }
+    Layout pobj = new Layout();
 
-    // Step 2 : Use that memory
-    iRet = SumEven(ptr,iLength);
-    
-    printf("Summation of even elements is : %d\n",iRet);
+     pobj.Display(iValue1, iValue2);
 
-    // Step 3 : Free the memory 
-    free(ptr);
 
-    return 0;
 }
+        
+}
+     
