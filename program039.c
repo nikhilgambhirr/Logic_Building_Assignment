015f4240d13ca4a823134893b6d0a0560ff7e700
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:52:54 +0530

    Update program039.c

--- program039.c
+++ program039.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
-// 
-void Display(int iNo)
-{
-    int iCnt = 0;
-    
-    for(iCnt = iNo; iCnt >= 1; iCnt--)
-{
-     printf("%d\n",iCnt);
+
+double FhtoCs(float fTemp)
+{  
+  
+  return (fTemp -32) * (5.0 / 9.0);
+   
 }
-}
+
 int main()
 {
-    int iValue = 0;
+      float fValue = 0.0;
+      double dRet = 0.0;
 
-  
-    printf(" Entear the frequency :\n");
-    scanf("%d",&iValue);
-    Display(iValue);
+      printf("Enter  temperatur in Fahrenheit:");
+      scanf("%f",&fValue);
 
-    return 0;
+      dRet = FhtoCs(fValue);
+
+      printf("Temperature in Celsius : %lf\n",dRet);
+
+      return 0;
 }
+
+/*
+    input:- 10
+    output:--12.2222  (10 - 32) * (5/9)
+*/
