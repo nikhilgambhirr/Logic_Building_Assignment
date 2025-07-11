10105845400eabe7bd64464682bab3a027dc563a
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:39:29 +0530

    Update program017.c

--- program017.c
+++ program017.c
@@ -1,38 +1,35 @@
-// Input : 4    4
-/*
-    @   @   @   @   
-    @   @   @   @   
-    @   @   @   @   
-    @   @   @   @   
-*/
-
+////////////////////////////////////////////
+// 
+//   File name: Program.c   
+//   Desription : factor in decreasing order.
+//   Author : Nikhil Kailas Gambhir
+//   Date : 15/05/2025
+//////////////////////////////////////////
 #include<stdio.h>
 
-void Display(int iRow, int iCol)
+void FactRev(int iNo)
 {
-    int i = 0, j = 0;
+  int iCnt = 0;
 
-    for(i = 1; i<= iRow; i++)
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
     {
-        for(j = 1; j <= iCol; j++)
+        if((iNo % iCnt )== 0)
         {
-            printf("@\t");
+            printf("%d\n",iCnt);
         }
-        printf("\n");
     }
+     
 }
+
 
 int main()
 {
-    int iValue1 = 0, iValue2 = 0;
+    int iValue =0;
 
-    printf("Enter number of rows : \n");
-    scanf("%d",&iValue1);
+    printf("Enter number:");
+    scanf("%d",&iValue);
 
-    printf("Enter number of columns : \n");
-    scanf("%d",&iValue2);
-
-    Display(iValue1, iValue2);
-
+    FactRev(iValue);
     return 0;
+
 }
