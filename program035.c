d8294747c40afdf8f0a32c5a18e5a6301e1e19be
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:51:07 +0530

    Update program035.c

--- program035.c
+++ program035.c
@@ -1,22 +1,39 @@
 #include<stdio.h>
-// iteration
-void Display(int iNo)
+
+int FactorialDiff(int iNo)
 {
-    int iCnt = 0;
-    
-    for(iCnt = 1; iCnt <=5; iCnt++)
-{
-     printf("%d\n",iNo);
-}
+
+     int iCnt =0;
+     int evenFact= 1, oddFact = 1;
+
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+
+     for(iCnt = 1; iCnt <= iNo; iCnt++)
+     {
+             if(iCnt % 2 == 0)
+             {
+                evenFact *= iCnt;
+             } 
+             else
+             {
+                 oddFact *= iCnt;
+             }
+     }
+    return evenFact-oddFact;
 }
 int main()
 {
-    int iValue = 0;
+    int iValue = 0, iRet =0;
+     
+     printf("enter number :");
+     scanf("%d",&iValue);
 
-  
-    printf(" Entear your value \n");
-    scanf("%d",&iValue);
-    Display(iValue);
+     iRet = FactorialDiff(iValue);
+     printf(" Factorial Difference is %d",iRet);
 
-    return 0;
+     return 0;
+
 }
