8c4490ef1bc6e0ab8969e3e47409f48a0268f0cb
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:14:41 +0530

    Add files via upload

---
+++ program027.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool chek (int iNo)
+{
+    int iCnt = 0 , isum = 0;
+   if( iNo < 0)
+   {
+    iNo = -iNo;
+   } 
+
+    for( iCnt = 1; iCnt<=( iNo / 2); iCnt++)
+  {
+    
+  }
+}
+int main()
+{
+     int ivalue = 0;
+     bool bRet = false; 
+
+     printf("Enter number :\n");
+     scanf("%d",&ivalue);
+
+     if(bRet == true)
+     {
+        printf("%d is perfact number");
+     }
+
+}
