a1bd5376dcbfd6095526fbdf765745cf71a6b043
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:47:43 +0530

    Update program029.c

--- program029.c
+++ program029.c
@@ -1,53 +1,24 @@
 #include<stdio.h>
-#include<stdbool.h>
-
-bool CheckPrime(int iNo)
+void Table(int iNo)
 {
     int iCnt = 0;
-    
 
-    if(iNo < 0)
+    for(iCnt = 1; iCnt<= 10; iCnt++)
     {
-        iNo = -iNo;
+         printf("%d\n",iCnt * -iNo);
     }
 
-    for( iCnt = 2; iCnt <= (iNo / 2); iCnt++)
-    {
-        if(( iNo % iCnt) == 0)
-        {
-        
-            break;
-        }
-    }                       // bFlag n use karta ha program type kela ahe tyat prime  number ahe ka nay te sangt 
-    if( iCnt> (iNo/2))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
 }
-
 
 int main()
 {
     int iValue = 0;
-    bool bRet = false;
 
-    printf("Enter number : \n");
+    printf("Enter number:");
     scanf("%d",&iValue);
 
-    bRet = CheckPrime(iValue);
-
-    if(bRet == true)
-    {
-        printf("%d is prime number\n",iValue);
-    }
-    else
-    {
-        printf("%d is not a prime number\n",iValue);
-    }
+    Table(iValue);
 
     return 0;
+    
 }
