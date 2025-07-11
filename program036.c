53675abae3acc7892c050b49d3967ae22ad5e768
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:15:54 +0530

    Add files via upload

---
+++ program036.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+// iteration
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 1; iCnt <=iNo; iCnt++)
+{
+     printf("Hello\n");
+}
+}
+int main()
+{
+    int iValue = 0;
+
+  
+    printf(" Entear the frequency :\n");
+    scanf("%d",&iValue);
+    Display(iValue);
+
+    return 0;
+}
