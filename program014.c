04fc922b800fc7ea6072765c7bbb7168890ab595
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:37:12 +0530

    Update program014.c

--- program014.c
+++ program014.c
@@ -1,30 +1,35 @@
+/////////////////////////////////////////////
+// 
+//   File name: Program.c   
+//   Desription : 
+//   Author : Nikhil Kailas Gambhir
+//   Date : 10/05/2025
+//
+//////////////////////////////////////////////
 #include<stdio.h>
-
-void Display()
+#include<ctype.h> // he library ahe character sathi
+void DisplayConvret(char cValue)
 {
-    int j = 0;
+    if(islower(cValue))
+    {
+        printf("output : %c\n",toupper(cValue));// he lowercase word upper case madhe convert karte
+    }
+    else if(isupper(cValue))
+    {
+        printf("output : %c\n",tolower(cValue));// he uppercase che word lower case madhe covert karte
+    } 
+    else
+    {
+         printf("not an alphabet chsrscter.\n"); //ani he charactor nasel tr tas sangte
 
-    for(j = 1; j <= 3; j++)
-    {
-        printf("*\t");
     }
-    printf("\n");
-    for(j = 1; j <= 3; j++)
-    {
-        printf("*\t");
-    }
-    printf("\n");
-    for(j = 1; j <= 3; j++)
-    {
-        printf("*\t");
-    }
-    printf("\n");
 }
 
 int main()
 {
-
-    Display();
-
-    return 0;
-}
+     char cValue  = '\0';
+     printf("Enter character:\n");
+     scanf(" %c", &cValue);
+     DisplayConvret(cValue);
+     return 0;
+     }
