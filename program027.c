ac687d594434b80c9a6b4600becac20bd69d5667
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:47:01 +0530

    Update program027.c

--- program027.c
+++ program027.c
@@ -1,29 +1,32 @@
 #include<stdio.h>
-#include<stdbool.h>
-bool chek (int iNo)
+
+void Display(int iNo)  //
 {
-    int iCnt = 0 , isum = 0;
-   if( iNo < 0)
+   switch (iNo)
    {
-    iNo = -iNo;
-   } 
+    case 0:printf("Zero\n"); break;
+    case 1: printf("One\n"); break;
+    case 2: printf("Two\n"); break;
+    case 3: printf("Three\n"); break;
+    case 4: printf("Four\n");break;
+    case 5: printf("Five\n");break;
+    case 6: printf("Six\n");break;
+    case 7: printf("Seven\n");break;
+    case 8: printf("Eight\n");break;
+    case 9 :printf("Nine\n");break;
+    default:printf("Invalid Number\n");
 
-    for( iCnt = 1; iCnt<=( iNo / 2); iCnt++)
-  {
-    
-  }
+   }
 }
+
 int main()
-{
-     int ivalue = 0;
-     bool bRet = false; 
+{ 
+   int iValue = 0;
 
-     printf("Enter number :\n");
-     scanf("%d",&ivalue);
+   printf("Enter number:");
+   scanf("%d",&iValue);
 
-     if(bRet == true)
-     {
-        printf("%d is perfact number");
-     }
+    Display(iValue);
 
+       return 0;
 }
