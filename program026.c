051631e504865565eb28e7621236b94c577c3168
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:46:42 +0530

    Update program026.c

--- program026.c
+++ program026.c
@@ -1,43 +1,31 @@
 #include<stdio.h>
-#include<stdbool.h>
 
-bool CheckPrime(int iNo)
+void Number(int iNo)
 {
-    int iCnt = 0;
-    bool bFlag = true;
+  if( iNo <50)
+  {
+    printf("Small\n");
     
-    if(iNo < 0)     // Updator
-    {   iNo = -iNo; }
+  }
+  else if (iNo>=50 && iNo <= 100)
+  {
+    printf("Medium\n");
+  }
+  else
+  {
+    printf("Large\n");
+  }
 
-    for(iCnt = 2; iCnt <= iNo/2 ; iCnt++)
-    {
-        if((iNo % iCnt == 0))
-        {
-            bFlag = false;
-            break;
-        }
-    }
-    return bFlag;
 }
 
 int main()
 {
-    int iValue = 0;
-    bool bRet = false;
+   int iValue= 0;
 
-    printf("Enter number : \n");
-    scanf("%d",&iValue);
+   printf("Enter number");
+   scanf("%d",&iValue);
 
-    bRet = CheckPrime(iValue);
+   Number(iValue);
 
-    if(bRet == true)
-    {
-        printf("%d is prime number\n",iValue);
-    }
-    else
-    {
-        printf("%d is not a prime number\n",iValue);
-    }
-
-    return 0;
+   return 0;
 }
