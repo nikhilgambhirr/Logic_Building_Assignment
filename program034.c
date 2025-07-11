31adf367afca2bc6e7620f6ed15e47a8069cbf80
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-11 23:50:48 +0530

    Update program034.c

--- program034.c
+++ program034.c
@@ -1,17 +1,33 @@
 #include<stdio.h>
-//iteration 
-void Display()
-{
-    int iCnt = 0;
-    
-    for(iCnt = 1; iCnt <=4; iCnt++)
-{
-     printf("%d\n",iCnt);
-}
-}
-int main()
-{
-    Display();
 
-    return 0;
+int OddFactorical(int iNo)
+{
+
+     int iCnt =0;
+     int iFact= 1;
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+     for(iCnt = iNo; iCnt>= 1;iCnt--)
+     {
+             if(iCnt % 2 != 0)
+             {
+                iFact *= iCnt;
+             } 
+     }
+    return iFact; 
+}
+     int main()
+{
+     int iValue = 0, iRet =0;
+     
+     printf("enter number :");
+     scanf("%d",&iValue);
+
+     iRet = OddFactorical(iValue);
+     printf("Odd Factorical of number is %d",iRet);
+
+     return 0;
+
 }
