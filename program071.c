9652160a1b4c9047b6908a49a7a0b36d4ecd9bae
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-12 00:18:30 +0530

    Update program071.java

--- program071.c
+++ program071.c
@@ -1,64 +1,55 @@
+// input 4 4
+/*
+       
+      
+       
+   
+ */
+import java.util.*;
 
-#include<stdio.h>
-#include<stdlib.h>
-
-int Frequency(int Arr[], int iSize, int iNo)
+class Layout
 {
-    int iCnt = 0, iCount = 0;
+    public void Display(int iRow,int iCol)
+  {
+       int i = 0, j = 0;
 
-    if((Arr == NULL) || (iSize <= 0))
-    {
-        return -1;
+      for(i = 1; i <= iRow; i++)
+       {
+           for(j =1; j <= iCol; j++)
+           {
+             if( i >= j)
+             {
+                System.out.print("*\t");
+            
+             }
+             else
+             {
+                System.out.print("#\t");
+             }
+               
+           }
+       System.out.println("\n");
     }
-
-    for(iCnt = 0; iCnt < iSize; iCnt++)
-    {
-        if(Arr[iCnt] == iNo)
-        {
-            iCount++;
-        }
-    }
-
-    return iCount;
+  }   
 }
-
-int main()
+class program71
+ {
+     public static void main(String A[])
 {
-    int *iPtr = NULL;
-    int iLength = 0, iValue = 0, iCnt = 0, iRet = 0;
+    Scanner sobj = new Scanner(System.in);
+    int iValue1 = 0, iValue2 = 0;
 
-    printf("Enter number of elements : \n");
-    scanf("%d",&iLength);
+    System.out.println("Enter number of rows :");
+    iValue1 = sobj.nextInt();
 
-    iPtr = (int *)malloc(iLength * sizeof(int));
-    if(NULL == iPtr)
-    {
-        printf("Unable to allocate the memory\n");
-        return -1;
-    }
+    System.out.println("Enter number of colum :"); 
+    iValue2 = sobj.nextInt();
 
-    printf("Enter the elements : \n");
-    for(iCnt = 0; iCnt < iLength; iCnt++)
-    {
-        printf("\nEnter the number %d : ",iCnt+1);
-        scanf("%d",&iPtr[iCnt]);
-    }
+    Layout pobj = new Layout();
 
-    printf("\nEnter the value that you want to search : \n");
-    scanf("%d",&iValue);
+    pobj.Display(iValue1,iValue2);
 
-    iRet = Frequency(iPtr, iLength, iValue);
-
-    if(iRet == -1)
-    {
-        printf("There is issue in the input");
-    }
-    else
-    {
-        printf("%d is appears %d times in the array\n",iValue, iRet);
-    }
-
-    free(iPtr);
-
-    return 0;
 }
+        
+}
+     
