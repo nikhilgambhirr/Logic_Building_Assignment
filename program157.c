8236f502449d9064cc4c0f045a8bbccb0c2417ec
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-07-18 19:27:45 +0530

    Add files via upload

---
+++ program157.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef unsigned int UINT;
+
+ UINT OFFBit( UINT iNo , int pos )
+{
+   
+   UINT iMaxsk = 1; 
+   UINT iResult = 0;
+   
+   iMaxsk = iMaxsk << pos-1;
+   iMaxsk = ~iMaxsk;
+
+   iResult = iNo & iMaxsk;
+
+   return iResult;
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    int iRet = 0;
+    int location = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d",&iValue);
+
+    printf("Enter the Positin : \n");
+    scanf("%d",&location);
+
+    iRet = OFFBit(iValue,location);
+
+     printf("Updated the Number: %d\n",iRet);
+
+    
+    return 0;
+}
+    
