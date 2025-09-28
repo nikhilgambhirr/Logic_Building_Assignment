82fb9ac66ca34e874278062e2a7c1400aeca1ef7
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-09-28 13:09:57 +0530

    Add files via upload

---
+++ Assignment/Assignment_040/program197.js
@@ -0,0 +1,12 @@
+function Display()
+{
+    let i = 0, iCount = " ";
+
+    for(i = 1; i <= 5; i++)
+    {
+       iCount += i + " ";
+    }
+    console.log(iCount);
+}
+
+Display();
---
+++ Assignment/Assignment_040/program198.js
@@ -0,0 +1,12 @@
+function Display()
+{
+    let i = 0, iCount = " ";
+
+    for(i = 5; i >= 1; i--) // 
+    {
+       iCount += i + " ";
+    }
+    console.log(iCount);
+}
+
+Display();
