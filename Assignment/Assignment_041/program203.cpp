4d655303fe575de7887c588e939cc9fd9e5893f5
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-09-28 13:14:23 +0530

    Add files via upload

---
+++ Assignment/Assignment_041/program203.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+void Display(T no)
+{  
+    if( no >= 1)
+    {
+        cout<<no<<"\t";
+        Display(--no);
+    }
+    
+}
+int main()
+{
+    int iValue = 0;
+    cout<<"Enter the Number :"<<"\n";
+    cin>>iValue;
+
+    Display(iValue);
+    
+    
+    return 0;
+}
