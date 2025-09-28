5c6b6eff43c6a992a610c51991c7b5ec05d7deb6
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-09-28 13:24:19 +0530

    Add files via upload

---
+++ Assignment/Assignment_050/program249.java
@@ -0,0 +1,69 @@
+/* input : irow 6       iCol : 6
+          *  *  *  *  *  *
+          *  #  #  #  *  * 
+          *  #  #   *  $ * 
+          *  #  *   $  $ *
+          *  *  $  $   $ *
+          *  *  *  *  *  *  
+*/
+
+import java.util.*;
+ 
+class  Layout
+{
+    public void Pattern(int iRow, int iCol)
+   {
+      int i = 0, j = 0;
+      int Arr = 0;
+
+     for ( i = 0; i < iRow; i++)
+     {
+        for (j = 0; j < iCol; j++)
+        { 
+          if( i == 0 || i == iRow -1 || j == 0 || j == iCol -1)
+          {
+              System.out.print("* ");
+          }
+          else if(i + j == iCol-1) 
+          {
+              System.out.print("* ");
+          }
+          else if(i + j < iCol - 1)
+          {
+            System.out.print("# ");
+          }
+          else
+          {
+            System.out.print("$ ");
+          }
+          
+       }
+       System.out.println();
+
+     }
+   }  
+
+} 
+
+
+class program249
+{
+    public static void main(String A[] ) 
+    {
+         Scanner sobj = new Scanner(System.in);
+
+         int row = 0, col = 0;
+            
+         System.out.println("Enter Row : ");
+          row = sobj.nextInt();
+
+         System.out.print("Enter Column : \n");
+          col = sobj.nextInt();
+
+         Layout lobj = new Layout();
+         lobj.Pattern(row, col);
+
+         sobj.close();
+
+    }
+}
