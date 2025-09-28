f2ca0000d45c98c744e23269e26e7fd817d11635
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-09-28 12:15:44 +0530

    Add files via upload

---
+++ Assignment/Assignment_001/program5.c
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////
+// 
+//   File name: Program.c   
+//   Desription : 
+//   Author : Nikhil Kailas Gambhir
+//   Date : 8/05/2025
+//
+/////////////////////////////////////////////////
+
+#include<stdio.h>
+void Accept(int iNo)
+{
+   int iCnt = 0;
+   for( iCnt = 1; iCnt <= iNo; iCnt++)
+{  
+    printf("10\n");
+     
+}
+
+}
+  int main()
+  {
+     int iValue = 0;
+     iValue = 5;
+
+     Accept(iValue);
+     return 0;
+  }
