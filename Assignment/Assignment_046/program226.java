c8f9221f24e3e2ba9ab1815f00bffdbc7c8c04f8
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-09-28 13:19:50 +0530

    Add files via upload

---
+++ Assignment/Assignment_046/program226.java
@@ -0,0 +1,51 @@
+/* input : irow 4        iCol : 4
+           A  B   C   D
+           A  B   C   D
+           A  B   C   D
+           A  B   C   D
+
+*/
+
+import java.util.*;
+ 
+class Layout
+{
+    public void Pattern(int iRow, int iCol)
+   {
+      int i = 0, j = 0;
+      int Arr = 0;
+
+     for ( i = 0; i < iRow; i++)
+     {
+        for (j = 0; j < iCol; j++)
+        {
+            System.out.print((char)('A' + j) + "  ");
+        }
+        System.out.println();
+     }
+
+  }
+
+} 
+
+class program226
+{
+    public static void main(String A[] ) 
+    {
+         Scanner sobj = new Scanner(System.in);
+
+         int row = 0, col = 0;
+            
+         System.out.println("Enter Row : ");
+          row = sobj.nextInt();
+
+         System.out.print("Enter Column : \n");
+          col = sobj.nextInt();
+
+         Layout lobj = new Layout();
+         lobj.Pattern(row, col);
+
+         sobj.close();
+
+    }
+}
