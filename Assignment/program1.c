0e1684df67b9c6156826779ddd4e2cfbe13fb359
nikhilgambhirr <nikhilgambhirr@gmail.com>
2025-09-28 12:12:44 +0530

    Add files via upload

---
+++ Assignment/program1.c
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////
+// 
+//   File name: Program.c   
+//   Desription : 
+//   Author : Nikhil Kailas Gambhir
+//   Date : 8/05/2025
+//
+///////////////////////////////////////////
+
+#include<stdio.h>
+
+int Divide(int iNo1, int iNo2)
+{
+    int iAns = 0;
+
+    if(iNo2== 0)
+    {
+        return -1;
+    }
+    iAns = iNo1 / iNo2;
+    return iAns;
+}
+    int main()
+    {
+         int ivalue1 = 15,  ivalue2 = 5;
+         int iRet = 0;
+
+         iRet = Divide(ivalue1,ivalue2);
+
+         printf("Division is %d", iRet);
+         return 0;
+
+    }
---
+++ Assignment/program2.c
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////
+// 
+//   File name: Program.c   
+//   Desription : 
+//   Author : Nikhil Kailas Gambhir
+//   Date : 8/05/2025
+//
+/////////////////////////////////////
+
+#include<stdio.h>
+void Display()
+
+{
+   int i = 0;
+   for(i = 1; i<=5;i++)
+   {
+     printf("Marvellous\n");
+   }
+}
+    int main()
+    {
+         Display();
+         return 0;
+
+    }
